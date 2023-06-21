3fed452951d56bdab12c24e8ec87100a4cefb8dd
utkarsh-gupta3107 <84470116+utkarsh-gupta3107@users.noreply.github.com>
2023-06-22 02:16:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Task/>}>
+        <Route path='/' element={</>}>
         </Route>
         
       </Routes></BrowserRouter>
